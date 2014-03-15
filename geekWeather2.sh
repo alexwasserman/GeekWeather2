662f13deb0eddcb35f72a6fc434362870a9d60ee
Alex Wasserman <alex@Smiley.local>
2014-03-14 23:08:33 -0400

    Updated to use webkit2png

--- geekWeather2.sh
+++ geekWeather2.sh
@@ -39,23 +39,19 @@ fi
 
 cd `dirname $0`
 
-export TEMPLATE_HTML='<iframe id="forecast_embed" type="text/html" frameborder="0" height="275" width="550" src="http://forecast.io/embed/#lat=$LAT&lon=$LON&name=$NAME"></iframe>'
+export TEMPLATE_URL='http://forecast.io/embed/#lat=$LAT&lon=$LON&name=$NAME'
 
-export HTML=$(echo $TEMPLATE_HTML | sed -e "s/\$LAT/$1/" -e "s/\$LON/$2/" -e "s/\$NAME/$3/")
-
-echo $HTML > /tmp/geekweather2.html
+export URL=$(echo $TEMPLATE_URL | sed -e "s/\$LAT/$1/" -e "s/\$LON/$2/" -e "s/\$NAME/$3/")
 
 echo "Converting to image"
-wkhtmltoimage --enable-javascript --javascript-delay 3000 --width 550 /tmp/geekweather2.html /tmp/tmp1.png
+webkit2png --width=300 --height=150 -F --transparent --delay=5 -o tmpWeather -D /tmp $URL
 
 echo "Running image smoothing"
 if [[ $4 == "LIGHT" ]]; then
-	convert -quiet -negate png:/tmp/tmp1.png -fuzz 30% -transparent black -fill '#CCC' -fuzz 60% -opaque '#888' -fuzz 40% -fill white -opaque black -blur 0x.4 png:/tmp/weather.png
+	convert -quiet -negate png:/tmp/tmpWeather-full.png png:/tmp/GeekWeather.png
+	rm /tmp/tmpWeather-full.png
 elif [[ $4 == "DARK" ]]; then
-	convert -quiet png:/tmp/tmp1.png -fuzz 30% -transparent white -fuzz 30% -fill black -opaque white -blur 0x.4  png:/tmp/weather.png
+	mv /tmp/tmpWeather-full.png /tmp/GeekWeather.png
 fi
-
-# rm /tmp/tmp1.png
-rm /tmp/geekweather2.html
 
 exit 0
