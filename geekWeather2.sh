53fbef64a9977a48f87fe9ba70c78b3e2fad24dc
David Patterson <bob0the0mighty@gmail.com>
2014-08-05 10:55:04 -0400

    fixed order of args in url

--- geekWeather2.sh
+++ geekWeather2.sh
@@ -34,14 +34,14 @@ cd `dirname $0`
 
 export TEMPLATE_URL='http://forecast.io/embed/#lat=$LAT&lon=$LON&name=$NAME&font=$FONT&units=$UNITS'
 
-export URL=$(echo $TEMPLATE_URL | sed -e "s/\$LAT/$1/" -e "s/\$LON/$2/" -e "s/\$NAME/$3/" -e "s/\$FONT/$4/" -e "s/\$UNITS/$5/" )
+export URL=$(echo $TEMPLATE_URL | sed -e "s/\$LAT/$1/" -e "s/\$LON/$2/" -e "s/\$NAME/$3/" -e "s/\$UNITS/$4/" -e "s/\$FONT/$5/")
 
 echo "Converting to image"
-webkit2png --width=500 --clipwidth=500 --height=245 --scale=1 -F --transparent --delay=5 -o tmpWeather -D /tmp $URL
+webkit2png -z 2 -F --transparent --delay=5 -o tmpWeather -D /tmp $URL
 
 echo "Running image smoothing"
 if [[ $6 == "LIGHT" ]]; then
-    convert -quiet -negate png:/tmp/tmpWeather-full.png png:/tmp/GeekWeather.png
+    convert -quiet -negate -modulate 150 png:/tmp/tmpWeather-full.png png:/tmp/GeekWeather.png
     rm /tmp/tmpWeather-full.png
 elif [[ $6 == "DARK" ]]; then
     mv /tmp/tmpWeather-full.png /tmp/GeekWeather.png
